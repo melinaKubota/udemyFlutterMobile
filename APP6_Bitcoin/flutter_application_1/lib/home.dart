8d35596ba4aa2d0361d1af93e043ecb60f9643a6
Melina Kubota <melina.kubota@gmail.com>
2021-07-26 22:33:42 -0300

    app

--- APP6_Bitcoin/flutter_application_1/lib/home.dart
+++ APP6_Bitcoin/flutter_application_1/lib/home.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
-  const ({ Key? key }) : super(key: key);
-
   @override
-  _State createState() => _State();
+  _HomeState createState() => _HomeState();
 }
 
-class _State extends State<> {
+class _HomeState extends State<Home> {
+
+  String _preco = "0";
+
+  void _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+    http.Response response = await http.get(url);
+
+    Map<String, dynamic> retorno = json.decode( response.body );
+    print("Resultado: " + retorno.["BRL"] ["buy"].toString());
+      _preco = retorno["BRL"] ["buy"].toString();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(padding: EdgeInsets.all(32),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: Center,
+          children: <Widget>[
+            Image.asset("images/bitcoin.png"), 
+            Padding(
+              padding: EdgeInsets.only(top:30, bottom:30),
+              child: Text (
+              "R\$" + _preco
+              style: TextStyle(
+                fontSize: 35
+                ),
+              ),
+            )
+            RaisedButton(child: Text(
+              "Atualizar"
+              style: TextStyle: 20, 
+              color: Colors.white,
+            ), 
+            )
+            color: Colors.orange, 
+            Padding: EdgeInsets.fromLTRB(30, 15, 30, 15)
+            onPressed: _recuperarPreco(),
+          ],
+        ),
+      ),
+     ),
     );
   }
 }
