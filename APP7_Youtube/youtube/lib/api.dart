d324c42c9281fae66a5e8bde04d0e926c6b92530
Melina Kubota <melina.kubota@gmail.com>
2021-07-28 14:51:42 -0300

    Convertendo os videos em lista

--- APP7_Youtube/youtube/lib/api.dart
+++ APP7_Youtube/youtube/lib/api.dart
@@ -1,6 +1,8 @@
 import 'package:sky_engine/_http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:youtube/model/video.dart';
+
 const CHAVE_YOUTUBE_API = "AIzaSyA9xcv5Ny8vs7tQ8JLMgAHozRubk2v7Rh8";
 const ID_CANAL = "F4WCRWP3XEI";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
@@ -8,21 +10,32 @@ const URL_BASE = "https://www.googleapis.com/youtube/v3/";
 class Api {
   pesquisar(String pesquisa) async {
     http.HttpResponse response = await http.get(
-    //http.Response response = await http.get(
-      URL_BASE + "search"
-      "?part=snippet" //no primeiro eu coloco? nos outros coloco &
-      "&type=video"
-      "&maxResults=20"
-      "&order=date"
-      "&key=$CHAVE_YOUTUBE_API"
-      "&chanelId==$ID_CANAL"
-      "&q=$pesquisa"
-      );
-      if( response.statusCode == 200){
-        Map<String, dynamic> dadosJson = json.decode(response.body);
-        print("Resultado: " + dadosJson["items"][0]["snippet"] ["title"].toString() );
-      }else{
+        //http.Response response = await http.get(
+        URL_BASE +
+            "search"
+                "?part=snippet" //no primeiro eu coloco? nos outros coloco &
+                "&type=video"
+                "&maxResults=20"
+                "&order=date"
+                "&key=$CHAVE_YOUTUBE_API"
+                "&chanelId==$ID_CANAL"
+                "&q=$pesquisa");
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dadosJson = json.decode(response.body);
 
+      List<Video> videos = dadosJson["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      for (var video in videos) {
+        print("Resultado: " + video.titulo);
       }
+
+      /*for (var video in dadosJson["items"]) {
+        print("Resultado: " + video.toString());
+      }*/
+      print(
+          "Resultado: " + dadosJson["items"][0]["snippet"]["title"].toString());
+    } else {}
   }
 }
--- APP7_Youtube/youtube/lib/model/video.dart
+++ APP7_Youtube/youtube/lib/model/video.dart
@@ -4,6 +4,19 @@ class Video {
   String imagem;
   String canal;
 
+  Video(
+      {required this.id,
+      required this.titulo,
+      required this.imagem,
+      required this.canal});
 
-  Video({required this.id, required this.titulo, required this.imagem, required this.canal});
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"] ["thumbnails"] ["high"] ["url"],
+      canal: json["snippet"]["channelId"],
+      );
+
+  }
 }
