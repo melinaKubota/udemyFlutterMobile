fc8ebb4406619b1a22f64e941b85094f6dbaf7ed
Andressa Albuquerque <andressa.dsa2@gmail.com>
2021-08-05 12:33:10 -0300

    Arrumando versão do Firebase para web

--- Firebase/firebase/lib/main.dart
+++ Firebase/firebase/lib/main.dart
@@ -1,56 +1,66 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   FirebaseFirestore db = FirebaseFirestore.instance;
-  /* db.collection("usuarios").document("001").setData({
+  db.collection("usuarios").doc("001").set({
     //salva ou atualiza, basta colocar o numero certo
     "nome": "Melina",
     "idade": "28",
   });
- */
-  /* DocumentReference ref = await db
-      .collection("noticias")
-      .add({"titulo": "Ondas de calor", "descricao": "texto de exemplo..."}); */
-  //print("item salvo: " + ref.documentID );
 
-/*   db.collection("Noticias")
-  .document(" ")
-  .setData("titulo" : "Ondas de calor em São Paulo",
-  "descricao" : "texto base"); */
+  DocumentReference ref = await db.collection("noticias").add({
+    "titulo": "Ondas de calor",
+    "descricao": "texto de exemplo...",
+  });
+
+  print("item salvo: " + ref.id);
+
+  db.collection("noticias").doc(" ").set({
+    "titulo": "Ondas de calor em São Paulo",
+    "descricao": "texto base",
+  });
 
   //deletar usuario
-  /* db.collection("usuarios").document("001").delete(); */
+  db.collection("usuarios").doc("001").delete();
 
   //copiar dados do banco de dados
-  /* DocumentSnapshot snapshot =
-    await db.collection("usuarios").document("001").get();
+  DocumentSnapshot snapshot = await db.collection("usuarios").doc("001").get();
 
   print("Dados:" + snapshot.data.toString());
 
-  var dados = snapshot.data;
-  print("dados: " + dados["nome"] + "idade:" + dados["idade"]); */
+  if (snapshot.data() != null) {
+    Map<String, dynamic> dados = snapshot.data as Map<String, dynamic>;
+    print("dados: " + dados["nome"] + "idade:" + dados["idade"]);
+  }
+  QuerySnapshot querySnapshot = await db.collection("usuarios").get();
+  print("Dados usuarios: " + querySnapshot.docs.toString());
 
-  /* QuerySnapshot querySnapshot = await db.collection("usuarios").getDocuments();
-  print("Dados usuarios: " + querySnapshot.documents.toString());
+  for (DocumentSnapshot item in querySnapshot.docs) {
+    Map<String, dynamic> dados = item.data as Map<String, dynamic>;
+    print("Dados usuarios: " + dados["Nome"] + "-" + dados["idade"]);
+  }
 
-  for (DocumentSnapshot item in querySnapshot.documents){
-    var dados = item.data;
-    print("Dados usuarios: " + dados["Nome"] + "-" + dados ["idade"]);
-  } */
-
-  /* db.collection("usuarios").snapshots().listen(snapshot){
-    for (DocumentSnapshot item in snapshot.documents){
-      var dados = item.data;
+  db.collection('usuarios').snapshots().listen((event) {
+    for (DocumentSnapshot item in snapshot.data()) {
+      Map<String, dynamic> dados = item.data as Map<String, dynamic>;
       print("dados usuarios:" + dados["nome"] + "-" + dados["idade"]);
     }
-  }
-  ); */
+  });
+
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('Hello world!'),
+    );
   }
 }
